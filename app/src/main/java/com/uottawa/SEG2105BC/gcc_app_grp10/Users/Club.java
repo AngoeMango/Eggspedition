d4cf7dd4331ddce2176025b367674ea0e62769d1
Angeleeca Jocson <angeleeca.jocson@gmail.com>
2023-10-10 16:23:16 -0400

    Update Club.java

--- app/src/main/java/com/uottawa/SEG2105BC/gcc_app_grp10/Users/Club.java
+++ app/src/main/java/com/uottawa/SEG2105BC/gcc_app_grp10/Users/Club.java
@@ -2,15 +2,15 @@ package com.uottawa.SEG2105BC.gcc_app_grp10.Users;
 
 import android.graphics.drawable.Drawable;
 import android.icu.text.MessagePattern;
+import java.util.ArrayList;
 
-import java.util.HashMap;
 
 
 public class Club extends User{
     /**
      * hashmap mutable so we can add more as needed, and easy to search for a particular one
      */
-    private HashMap<String,Participant> members;
+    private ArrayList<Participant> members;
 
     public Club(String username, String password, String email, String bio){
         super(Role.CLUB, username, password, email, bio);
@@ -24,8 +24,8 @@ public class Club extends User{
      * this method is going to need some protection
      * probably the addmember method above it too
      */
-    public void removeMember(String username){
-        members.remove(username);
+    public void removeMember(Participant member){
+        members.remove(member.getUsername());
     }
 
 
