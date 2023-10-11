b993a9c9e99dd433e569eeae78436068f8208d23
christian <ckp1091@gmail.com>
2023-10-10 22:58:11 -0400

    Add bio in admin class

--- app/src/main/java/com/uottawa/SEG2105BC/gcc_app_grp10/Users/Admin.java
+++ app/src/main/java/com/uottawa/SEG2105BC/gcc_app_grp10/Users/Admin.java
@@ -2,8 +2,8 @@ package com.uottawa.SEG2105BC.gcc_app_grp10.Users;
 
 public class Admin extends User{
 
-    public Admin(String username, String password, String email, String firstName){
-        super(Role.ADMIN, username, password, email, null, firstName);
+    public Admin(String username, String password, String email, String bio, String firstName){
+        super(Role.ADMIN, username, password, email, "Admin account", firstName);
     }
 
  }
