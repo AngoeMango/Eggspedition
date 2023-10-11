b5aabe2e6d3c21c461653732c6776b8557743659
christian <ckp1091@gmail.com>
2023-10-10 23:00:00 -0400

    Remove partially bio in admin class

--- app/src/main/java/com/uottawa/SEG2105BC/gcc_app_grp10/Users/Admin.java
+++ app/src/main/java/com/uottawa/SEG2105BC/gcc_app_grp10/Users/Admin.java
@@ -2,7 +2,7 @@ package com.uottawa.SEG2105BC.gcc_app_grp10.Users;
 
 public class Admin extends User{
 
-    public Admin(String username, String password, String email, String bio, String firstName){
+    public Admin(String username, String password, String email, String firstName){
         super(Role.ADMIN, username, password, email, "Admin account", firstName);
     }
 
