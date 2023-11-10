2e3e003c50b64e33d11124e3d1abfce3339728af
Angeleeca Jocson <angeleeca.jocson@gmail.com>
2023-11-10 08:13:53 -0500

    changed tools context for activity_admin_control

---
+++ app/src/main/java/com/uottawa/SEG2105BC/gcc_app_grp10/Activities/AdminControlPanel.java
@@ -0,0 +1,34 @@
+package com.uottawa.SEG2105BC.gcc_app_grp10.Activities;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.uottawa.SEG2105BC.gcc_app_grp10.Database.DatabaseHandler;
+import com.uottawa.SEG2105BC.gcc_app_grp10.R;
+
+public class AdminControlPanel extends AppCompatActivity {
+
+    DatabaseHandler databaseHandler;
+    private Button buttonViewEventTypes;
+
+    private Button buttonViewEvents;
+
+    private Button buttonViewAccounts;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_control_panel);
+
+        buttonViewEventTypes = (Button) findViewById(R.id.buttonViewEventTypes);
+        buttonViewEventTypes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openViewEventTypes();
+            }
+        });
+
+    }
+}
